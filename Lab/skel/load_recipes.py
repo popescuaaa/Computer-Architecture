7b881c8f9ff6e3ea49121c711203488e5d3d83e5
Popescu Andrei <andreigabrielpopescu55^@gmail.com>
2019-12-12 16:51:52 +0200

    Added Lab folder for 2019

---
+++ Lab/skel/load_recipes.py
@@ -0,0 +1,14 @@
+"""
+	Bonus task: load all the available coffee recipes from the folder 'recipes/'
+	File format:
+		first line: coffee name
+		next lines: resource=percentage
+
+	info and examples for handling files:
+		http://cs.curs.pub.ro/wiki/asc/asc:lab1:index#operatii_cu_fisiere
+		https://docs.python.org/2/library/io.html
+		https://docs.python.org/2/library/os.path.html
+"""
+
+RECIPES_FOLDER = "recipes"
+
