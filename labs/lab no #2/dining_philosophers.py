7d646b5bf3524ceb8f64ddd66b5f163623ec7735
Andrei Popescu <andreipopescu@Andreis-MacBook-Pro.local>
2020-02-25 19:03:31 +0200

    finished philosophers problem using sempaphores; rethink about the posibility with locks

--- labs/lab no #2/dining_philosophers.py
+++ labs/lab no #2/dining_philosophers.py
@@ -1,67 +1,68 @@
-import threading
-import random
-import time
- 
-# Dining philosophers, 5 Phillies with 5 forks. Must have two forks to eat.
-#
-# Deadlock is avoided by never waiting for a fork while holding a fork (locked)
-# Procedure is to do block while waiting to get first fork, and a nonblocking
-# acquire of second fork.  If failed to get second fork, release first fork,
-# swap which fork is first and which is second and retry until getting both.
-#  
-# See discussion page note about 'live lock'.
- 
-class Philosopher(threading.Thread):
- 
-    running = True
- 
-    def __init__(self, xname, forkOnLeft, forkOnRight):
-        threading.Thread.__init__(self)
-        self.name = xname
+from threading import Thread, Lock
+from random import randint, uniform, seed
+from time import sleep
+
+
+FULL = 'full'
+HUNGRY = 'hungry'
+
+class Philosopher(Thread):
+    
+    # base statisfiability constratint per instance
+    __status = HUNGRY
+
+    def get_status(self):
+        return self.__status
+    
+    def set_status(self, new_status):
+        self.__status = new_status
+
+    def __init__(self, name, forkOnLeft, forkOnRight):
+        Thread.__init__(self)
+        self.name = name
         self.forkOnLeft = forkOnLeft
         self.forkOnRight = forkOnRight
  
     def run(self):
-        while(self.running):
-            #  Philosopher is thinking (but really is sleeping).
-            time.sleep( random.uniform(3,13))
-            print('%s is hungry.' % self.name)
+        while self.__status == HUNGRY:
+            print('{} is hungry.'.format(self.name))
+            sleep(10)
             self.dine()
  
     def dine(self):
         fork1, fork2 = self.forkOnLeft, self.forkOnRight
- 
-        while self.running:
+        while self.__status == HUNGRY:
             fork1.acquire(True)
             locked = fork2.acquire(False)
-            if locked: break
+
+            if locked: 
+                break
             fork1.release()
-            print('%s swaps forks' % self.name)
+
+            print('{} swaps forks'.format(self.name))
             fork1, fork2 = fork2, fork1
         else:
             return
- 
+
         self.dining()
         fork2.release()
         fork1.release()
  
     def dining(self):			
-        print('%s starts eating '% self.name)
-        time.sleep(random.uniform(1,10))
-        print('%s finishes eating and leaves to think.' % self.name)
+        print('{} starts eating '.format(self.name))
+        for i in range(10): print('{} YUM YUM YUM YUM YUM YUM'.format(self.name))
+        print('{} finishes eating '.format(self.name))
  
-def DiningPhilosophers():
-    forks = [threading.Lock() for n in range(5)]
-    philosopherNames = ('Aristotle','Kant','Buddha','Marx', 'Russel')
- 
-    philosophers= [Philosopher(philosopherNames[i], forks[i%5], forks[(i+1)%5]) \
+def main():
+    forks = [Lock() for n in range(5)]
+    philosopher_names = ('Aristotle','Kant','Buddha','Marx', 'Russel')
+    philosophers= [Philosopher(philosopher_names[i], forks[i%5], forks[(i+1)%5]) \
             for i in range(5)]
- 
-    random.seed(507129)
-    Philosopher.running = True
+    seed(42)
     for p in philosophers: p.start()
-    time.sleep(100)
-    Philosopher.running = False
+    sleep(1000)
+    Philosopher.set_status(FULL)
     print ("Now we're finishing.")
  
-DiningPhilosophers()
+if __name__ == "__main__":
+     main()
