1e3b743a35d8914aa30a48c244905f0829e1c856
andrei.popescu1902 <andrei.popescu1902@dp-wn03.grid.pub.ro>
2020-03-21 19:17:37 +0200

    Finish the assignment session number 5 -> there is a pdf in which there are a couple of cool informations

--- labs/lab no#5/test_session.sh
+++ labs/lab no#5/test_session.sh
@@ -6,10 +6,13 @@ echo "Creating logile for the current session"
 touch logfile
 
 echo "Logfile created"
+echo "Current testing session" >> logfile
+echo "Evaluating the performance for basic, loop_reorder and cache_optimisation" >> logfile
+
 echo "========================================="
 
 echo "Started testing and performance evaluation"
 for script in basic loop_reorder cache_optimisation
 do
-   echo $script
+   ./$script >> logfile
 done
