82be7c988940f40d7e28c8331bdf2112ff4b80c7
Andrei Popescu <andreipopescu@192-168-0-104.rdsnet.ro>
2020-04-09 20:55:45 +0300

    Finished forgotten task 3 from lab 7

--- labs/lab no #7/task_3.cu
+++ labs/lab no #7/task_3.cu
@@ -18,11 +18,17 @@ int main(void) {
     // TODO 1: Allocate the host's arrays with the specified number of elements:
     // host_array_a => 32M
     // host_array_b => 32M
+    host_array_a = (float *) malloc(BUF_32M);
+    host_array_b = (float *) malloc(BUF_32M);
 
     // TODO 2: Allocate the device's arrays with the specified number of elements:
     // device_array_a => 32M
     // device_array_b => 32M
     // device_array_c => 2M
+
+    cudaMalloc( (void **) &device_array_a, BUF_32M);
+    cudaMalloc( (void **) &device_array_b, BUF_32M);
+    cudaMalloc( (void **) &device_array_c, BUF_2M);
 
     // Check for allocation errors
     if (host_array_a == 0 || host_array_b == 0 || 
@@ -44,11 +50,13 @@ int main(void) {
     }
 
     // TODO 3: Copy from host to device
+    cudaMemcpy(device_array_a, host_array_a, num_bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_array_b, host_array_b, num_bytes, cudaMemcpyHostToDevice);
 
     // TODO 4: Swap the buffers (BUF_2M values each iteration)
     // Hint 1: device_array_c should be used as a temporary buffer
     // Hint 2: cudaMemcpy
-
+    
     // TODO 5: Copy from device to host
 
     printf("\nAfter swap:\n");
