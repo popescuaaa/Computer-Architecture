c5384f4e40530ba73efe1b3da635734ac3e165f0
Andrei Popescu <andreipopescu@192-168-0-104.rdsnet.ro>
2020-04-29 12:40:42 +0300

    Resolved lab number 9

--- labs/lab no #9/lab9_skl/task4_dynp.cu
+++ labs/lab no #9/lab9_skl/task4_dynp.cu
@@ -9,79 +9,81 @@ using namespace std;
 
 // TODO
 // workers will compute sum on first N elements
-__global__ void worker()
+__global__ void worker(int *result, int *data, int pos)
 {
-    // TODO, compute sum and store in result
+	// TODO, compute sum and store in result
+	atomicAdd(&result[pos], data[threadIdx.x]);
 }
 
 // TODO
 // master will launch threads to compute sum on first N elements
-__global__ void master()
+__global__ void master(int *result, int *data)
 {
-    // TODO, schedule worker threads
+	// TODO, schedule worker threads
+	worker<<<1, data[threadIdx.x]>>>(result, data, threadIdx.x);
 }
 
 void generateData(int *data, int num) {
-    srand(time(0));
-    
-    for(int i = 0; i < num; i++) {
-        data[i] = rand() % 8 + 2;
-    }
+	srand(time(0));
+
+	for(int i = 0; i < num; i++) {
+		data[i] = rand() % 8 + 2;
+	}
 }
 
 void print(int *data, int num) {
-    for(int i = 0; i < num; i++) {
-        cout << data[i] << " ";
-    }
-    cout << endl;
+	for(int i = 0; i < num; i++) {
+		cout << data[i] << " ";
+	}
+	cout << endl;
 }
 
 // TASK check
 // each element result[i] should be sum of first data[i] elements of data[i]
 bool checkResult(int *data, int num, int *result) {
 
-    for(int i = 0; i < num; i++) {
-        
-        int sum = 0;
-        for(int j = 0; j < data[i]; j++) {
-            sum += data[j];
-        }
-        
-        if(result[i] != sum) {
-            cout << "Error at " << i << ", requested sum of first " 
-                << data[i] << " elem, got " << result[i] << endl;
-            return false;
-        }
-    }
-    
-    return true;
+	for(int i = 0; i < num; i++) {
+
+		int sum = 0;
+		for(int j = 0; j < data[i]; j++) {
+			sum += data[j];
+		}
+
+		if(result[i] != sum) {
+			cout << "Error at " << i << ", requested sum of first "
+				<< data[i] << " elem, got " << result[i] << endl;
+			return false;
+		}
+	}
+
+	return true;
 }
 
 int main(int argc, char *argv[])
 {
-    int *data = NULL;
-    cudaMallocManaged(&data, NUM_ELEM * sizeof(int));
+	int *data = NULL;
+	cudaMallocManaged(&data, NUM_ELEM * sizeof(int));
 
-    int *result = NULL;
-    cudaMallocManaged(&result, NUM_ELEM * sizeof(int));
-    
-    generateData(data, NUM_ELEM);
-    
-    // TODO schedule master threads and pass data/result/num 
-    master<<< 1, 1 >>>();
-    cudaDeviceSynchronize();
-    
-    print(data, NUM_ELEM);
-    print(result, NUM_ELEM);
-    
-    if(checkResult(data, NUM_ELEM, result)) {
-        cout << "Result OK" << endl;
-    } else { 
-        cout << "Result ERR" << endl;
-    }
-    
-    cudaFree(data);
-    cudaFree(result);
+	int *result = NULL;
+	cudaMallocManaged(&result, NUM_ELEM * sizeof(int));
 
-    return 0;
+	generateData(data, NUM_ELEM);
+
+	// TODO schedule master threads and pass data/result/num 
+	master<<<1, NUM_ELEM>>>(result, data);
+	cudaDeviceSynchronize();
+
+	print(data, NUM_ELEM);
+	print(result, NUM_ELEM);
+
+	if(checkResult(data, NUM_ELEM, result)) {
+		cout << "Result OK" << endl;
+	} else { 
+		cout << "Result ERR" << endl;
+	}
+
+	cudaFree(data);
+	cudaFree(result);
+
+	return 0;
 }
