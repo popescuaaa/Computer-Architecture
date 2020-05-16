7dfbece8cc4deb03af5e2fcb106bfbfcbeac8352
Andrei Popescu <andreipopescu@Andreis-MacBook-Pro.local>
2020-05-16 23:53:04 +0300

    debug <8>

--- homework no#3 2020/gpu_hashtable.cu
+++ homework no#3 2020/gpu_hashtable.cu
@@ -90,7 +90,7 @@ __global__ void kernelInsertEntry(int *keys, int *values, int *currentSize, int 
         int inplaceKey = atomicCAS(&hashTableBuckets[hash].HashTableEntryKey, KEY_INVALID, currentKey);
 
         if (inplaceKey == currentKey || inplaceKey == KEY_INVALID) {
-            if (inplaceKey == KEY_INVALID)
+            if (inplaceKey == currentKey)
                 atomicAdd(currentSize, 1);
             atomicExch(&hashTableBuckets[hash].HashTableEntryValue, currentValue);
             return;
