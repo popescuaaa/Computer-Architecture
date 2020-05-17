b567c30f992aeefe42fb5f809f9f7aaf804673c2
Andrei Popescu <andreipopescu@192-168-0-104.rdsnet.ro>
2020-05-17 12:44:07 +0300

    debug <21>

--- homework no#3 2020/gpu_hashtable.cu
+++ homework no#3 2020/gpu_hashtable.cu
@@ -138,12 +138,7 @@ __global__ void kernelInsertEntry(int *keys, int *values, int *currentSize, int 
     cout << threadBlockSize << endl;
     int gridSize = ceil(numKeys / threadBlockSize);
     cout << minGridSize << endl;
-
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    cudaEventRecord(start);
-
+    
     kernelInsertEntry<<< gridSize, threadBlockSize >>>(
             deviceKeys,
             deviceValues,
@@ -152,14 +147,10 @@ __global__ void kernelInsertEntry(int *keys, int *values, int *currentSize, int 
             hashTableBuckets,
             limitSize
             );
-            
-            
-    cudaEventRecord(stop);
-    cudaEventSynchronize(stop);
 
-    // cudaError_t cudaerr = cudaDeviceSynchronize();
-    // if (cudaerr != cudaSuccess)
-    //     printf("kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaerr));
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    if (cudaerr != cudaSuccess)
+        printf("kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaerr));
 
     cudaFree(deviceKeys);
     cudaFree(deviceValues);
