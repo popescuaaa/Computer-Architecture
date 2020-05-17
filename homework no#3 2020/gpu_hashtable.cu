10331f19c5e38b86f6f6f7ff5e0d73c5d09839e1
Andrei Popescu <andreipopescu@192-168-0-104.rdsnet.ro>
2020-05-17 12:57:15 +0300

    debug <26>

--- homework no#3 2020/gpu_hashtable.cu
+++ homework no#3 2020/gpu_hashtable.cu
@@ -136,7 +136,7 @@ __global__ void kernelInsertEntry(int *keys, int *values, int *currentSize, int 
     int threadBlockSize = 512;
     //cudaOccupancyMaxPotentialBlockSize(&minGridSize, &threadBlockSize, kernelInsertEntry, 0, 0);
     cout << threadBlockSize << endl;
-    int gridSize = ceil(numKeys + 1/ threadBlockSize);
+    int gridSize = ceil(numKeys/ threadBlockSize) + 1;
     cout << minGridSize << endl;
 
     kernelInsertEntry<<< gridSize, threadBlockSize >>>(
@@ -206,7 +206,7 @@ __global__ void kernelGetEntry( int *keys, int *values, int numKeys, int limitSi
     int threadBlockSize = 512;
     //cudaOccupancyMaxPotentialBlockSize(&minGridSize, &threadBlockSize, kernelInsertEntry, 0, 0);
 
-    int gridSize = ceil(numKeys + 1 / threadBlockSize);
+    int gridSize = ceil(numKeys / threadBlockSize) + 1;
 
     kernelGetEntry<<< gridSize, threadBlockSize >>>(
             deviceKeys,
